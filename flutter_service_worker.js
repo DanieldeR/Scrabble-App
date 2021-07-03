f737ab2751d956dcfa15942c3afbe65011ce48a8
DanieldeR <derepentigny.daniel@gmail.com>
2021-07-03 18:11:57 -0400

    Built web
    Branch: main
    Commit: 36249c513f67c8544f4c08667844df3495a7324c (dirty)
    
    package:peanut 4.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "289b5ad58e3b63a72c7f72cfa450fc1f",
-"index.html": "51ff8ab5feae3b23ba622e524bff47db",
-"/": "51ff8ab5feae3b23ba622e524bff47db",
+"index.html": "1a720c8efafd5371ea67491eb6d98bd7",
+"/": "1a720c8efafd5371ea67491eb6d98bd7",
 "main.dart.js": "3fcc7600e6bc6bbd6d353d9f722d3837",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
